a1bd9a0eaf0d465d03cc7078ba0372348246e7f9
Diana Lopez <diana.lopez04@uceva.edu.co>
2026-04-09 17:52:12 -0500

    feat(node): add swagger schemas for Book and Author

--- Server-NodeJS/src/config/swagger.schemas.ts
+++ Server-NodeJS/src/config/swagger.schemas.ts
@@ -71,4 +71,90 @@
  *           type: number
  *           example: 4500
  */
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       description: Representa un libro del sistema
+ *       required:
+ *         - id
+ *         - title
+ *         - author
+ *         - genre
+ *         - publisher
+ *       properties:
+ *         id:
+ *           type: number
+ *           example: 1
+ *         title:
+ *           type: string
+ *           example: Cien años de soledad
+ *         author:
+ *           type: string
+ *           example: Gabriel García Márquez
+ *         genre:
+ *           type: string
+ *           enum:
+ *             - Fantasia
+ *             - Ciencia Ficcion
+ *             - Drama
+ *             - Misterio
+ *             - Romance
+ *           example: Drama
+ *         publisher:
+ *           type: string
+ *           example: Sudamericana
+ */
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Author:
+ *       type: object
+ *       description: Representa un autor del sistema
+ *       required:
+ *         - id
+ *         - fullName
+ *         - gender
+ *         - bio
+ *         - birthdate
+ *         - zodiacSign
+ *       properties:
+ *         id:
+ *           type: number
+ *           example: 1
+ *         fullName:
+ *           type: string
+ *           example: Gabriel García Márquez
+ *         gender:
+ *           type: string
+ *           example: Male
+ *         bio:
+ *           type: string
+ *           example: Escritor colombiano, premio Nobel de Literatura
+ *         birthdate:
+ *           type: string
+ *           format: date
+ *           example: 1927-03-06
+ *         zodiacSign:
+ *           type: string
+ *           enum:
+ *             - Aries
+ *             - Tauro
+ *             - Geminis
+ *             - Cancer
+ *             - Leo
+ *             - Virgo
+ *             - Libra
+ *             - Escorpio
+ *             - Sagitario
+ *             - Capricornio
+ *             - Acuario
+ *             - Piscis
+ *           example: Piscis
+ */
 export {};
